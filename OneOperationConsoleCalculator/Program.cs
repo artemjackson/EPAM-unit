39d722c91a4d48eb0fbc22736dd1fa772788c4a9
Artem <artemjackson95@gmail.com>
2015-10-12 02:00:12 +0300

    Added main module

---
+++ OneOperationConsoleCalculator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SimpleCalculator;
+
+namespace OneOperationConsoleCalculator
+{
+	public class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length == 4)
+			{
+				int x = Int32.Parse(args [1]);
+				string op = args[2];
+				int y = Int32.Parse (args [3]);
+				
+				var calculator = new Calculator<double>();
+
+				try {
+					var answ = calculator.PerformOperation(x, op, y);
+					System.Console.WriteLine(answ);
+				} catch (DivideByZeroException exc){
+					System.Console.Write (exc.Message);
+				}
+
+			} else {
+				System.Console.WriteLine ("Input requires 3 parameters: int1, operation, int2");
+			}
+		}
+	}
+}
