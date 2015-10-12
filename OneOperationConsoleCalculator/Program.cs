adbac77f27d56290b9fce76fb07aa37f80c6d2fb
Artem <artemjackson95@gmail.com>
2015-10-12 13:20:38 +0300

    Applied better error handling

--- OneOperationConsoleCalculator/Program.cs
+++ OneOperationConsoleCalculator/Program.cs
@@ -9,21 +9,27 @@ namespace OneOperationConsoleCalculator
 		{
 			if (args.Length == 3)
 			{
-				int x = Int32.Parse(args [0]);
+				try {
+					int x = Int32.Parse(args [0]);
+					int y = Int32.Parse (args [2]);
+				} catch (FormatException exc){
+					System.Console.WriteLine("Invalid input parameters type. Parameters should be integers");
+				}
 				string op = args[1];
-				int y = Int32.Parse (args [2]);
-				
-				var calculator = new Calculator<double>();
+
+				var calculator = new Calculator<int>();
 
 				try {
 					var answ = calculator.PerformOperation(x, op, y);
 					System.Console.WriteLine(answ);
 				} catch (DivideByZeroException exc){
 					System.Console.Write (exc.Message);
+				} catch (UnknownOperationException exc) {
+					System.Console.Write (exc.Message);
 				}
 
 			} else {
-				System.Console.WriteLine ("Input requires 3 parameters: int1, operation, int2");
+				System.Console.WriteLine ("Invalid nuber of input parameters. Input requires 3 parameters: int1, operation, int2");
 			}
 		}
 	}
